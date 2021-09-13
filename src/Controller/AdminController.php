6ccd7bc908f89a1704e0138053f9fd0f794924c4
YannickB66 <78077144+YannickB66@users.noreply.github.com>
2021-09-13 15:23:19 +0200

    main Authentificator

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,7 +12,42 @@ class AdminController extends AbstractController
     #[Route('kgb/admin', name: 'admin')]
     public function index(): Response
     {
-        return $this->render('admin/index.html.twig', [
+
+
+        if(isset($_SESSION["admin"]))
+        {
+         if($_SESSION["admin"]== 'ok')
+         {
+             return $this->render('admin/index2.html.twig', [
+                 'controller_name' => 'AdminController',
+             ]);
+         }
+        }
+
+        if($_POST)
+        {
+            if($_POST["email"] === "ybruel@ymail.com")
+            {
+                if($_POST["password"] === "admin")
+                {
+
+                    if(!session_id())
+                    {
+                        session_start();
+                        session_regenerate_id();
+                        $_SESSION['admin'] = 'ok';
+                    }
+
+
+                    return $this->render('admin/index.html.twig', [
+                        'controller_name' => 'AdminController',
+                    ]);
+                }
+            }
+        }
+
+
+        return $this->render('admin/login.html.twig', [
             'controller_name' => 'AdminController',
         ]);
     }
